b960699cd00b69d8f4a2847d5a41a8eea24598c4
Max Afonov <max@bumnetworks.com>
2010-10-17 17:37:55 -0400

    SBT project

---
+++ project/build/CasbahMapperProject.scala
@@ -0,0 +1,22 @@
+import sbt._
+
+class CasbahMapperProject(info: ProjectInfo) extends DefaultProject(info) {
+  override def managedStyle = ManagedStyle.Maven
+  super.compileOptions ++ Seq(Unchecked, ExplainTypes, Deprecation)
+
+  val casbah_core = "com.novus" %% "casbah-core" % "1.1-SNAPSHOT"
+  val objenesis = "org.objenesis" % "objenesis" % "1.2"
+
+  val specs = "org.scala-tools.testing" %% "specs" % "1.6.5" % "test->default"
+  val commonsLang = "commons-lang" % "commons-lang" % "2.5" % "test->default"
+
+  val publishTo = Resolver.sftp("repobum", "repobum", "/home/public/%s".format(
+    if (projectVersion.value.toString.endsWith("-SNAPSHOT")) "snapshots"
+    else "releases"
+  )) as("repobum_repobum", new java.io.File(Path.userHome + "/.ssh/id_rsa"))
+
+  val scalaToolsRepo = "Scala Tools Release Repository" at "http://scala-tools.org/repo-releases"
+  val scalaToolsSnapRepo = "Scala Tools Snapshot Repository" at "http://scala-tools.org/repo-snapshots"
+  val bumRepo = "Bum Networks Release Repository" at "http://repo.bumnetworks.com/releases/"
+  val bumSnapsRepo = "Bum Networks Snapshots Repository" at "http://repo.bumnetworks.com/snapshots/"
+}
